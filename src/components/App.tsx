3d13c75869199aec5858b2517f3ff83635b57c79
Janika Hortizuela <jhortizuela21@gmail.com>
2021-12-03 21:20:14 -0700

    Set up files

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.scss';
+import Header from './Header'
+import Main from './Main'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+      aloha, i'm the menu
+    </div>
+  )
+}
+
+export default Menu
