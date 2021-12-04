e56601e0ac746874658c93f2ac9f57f124b03e61
Nolan Caine <nolancaine@gmail.com>
2021-12-04 01:33:53 -0700

    Add semicolons and example query hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// import '../styles/App.scss';
-import Header from './Header'
-import Main from './Main'
+
+import Header from './Header';
+import Main from './Main';
 
 const App = () => {
   return (
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_TODOS } from '../operations/queries/getAllTodos';
+import { Todos } from '../models/todo';
 
 const Main = () => {
+  const { data } = useQuery(GET_ALL_TODOS);
+	const todos: Todos = data.todos;
+
   return (
     <div>
       Hello
@@ -9,4 +14,4 @@ const Main = () => {
   )
 }
 
-export default Main
+export default Main;
