21543a58353d50c91d104221d67a064f45fb9af0
Janika Hortizuela <jhortizuela21@gmail.com>
2021-12-05 14:34:56 -0700

    complete block 1 styling

--- src/components/LandingBlock1.tsx
+++ src/components/LandingBlock1.tsx
@@ -6,9 +6,11 @@ const LandingBlock1 = () => {
   return (
     <div className="block1">
       <div className="videoContainer">
-
-        <h1 className="catchphrase">The smartest way to organize your research</h1>
-        <h2 className="catchphrase" id="sub">Never use bookmarks again!</h2>
+        <div className="description-1">
+          <h1 className="catchphrase">The smartest way to organize your research</h1>
+          <h2 className="catchphrase2">Never use bookmarks again!</h2>
+          <button className="block1-btn">Let's Go!</button>
+        </div>
         <div className="overlay"></div>
         <video autoPlay={true} muted loop >
           <source src={book} type="video/mp4" className="video"/>
