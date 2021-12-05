db28ac3e37e07d9878636e2d2a2e1e0c39abcac0
Nolan Caine <nolancaine@gmail.com>
2021-12-05 15:08:37 -0700

    Add https to uri

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient } from '@apollo/client';
 import cache from './cache';
 
 const client = new ApolloClient({
-  uri: 'thumbtack-api.herokuapp.com/graphql',
+  uri: 'https://thumbtack-api.herokuapp.com/graphql',
   connectToDevTools: true,
   cache
 });
