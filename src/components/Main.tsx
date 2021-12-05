0e88cfbe53f7ef0c6803b047bd4d8ec34b50c1c2
Nolan Caine <nolancaine@gmail.com>
2021-12-05 14:52:20 -0700

    Comment out unused query

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_ALL_TODOS } from '../operations/queries/getAllTodos';
-import { Todos } from '../models/todo';
+// import { GET_ALL_TODOS } from '../operations/queries/getAllTodos';
+// import { Todos } from '../models/todo';
 
 const Main = () => {
-  const { data } = useQuery(GET_ALL_TODOS);
-	const todos: Todos = data.todos;
+  // const { data } = useQuery(GET_ALL_TODOS);
+	// const todos: Todos = data.todos;
 
   return (
     <div>
