27c56a8057f62b2dce133a563f7fd70c420265e9
Nolan Caine <nolancaine@gmail.com>
2021-12-05 19:06:40 -0700

    Configure to open new tab

--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Resource as ResourceProps } from '../models/Resource';
 
@@ -6,9 +7,11 @@ const Resource = ({
   id, name, url, image, createdAt
 }: ResourceProps): JSX.Element => {
   return (
-    <article className='resource'>
-      <h3>{ name }</h3>
-    </article>
+    <Link to={ url } target="_blank" rel="noopener noreferrer">
+      <article className='resource'>
+        <h3>{ name }</h3>
+      </article>
+    </Link>
   )
 }
 
