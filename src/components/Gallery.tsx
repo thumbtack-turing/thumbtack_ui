d499a7c3b877105c2edf0c69b43e82bd4782c14b
Nolan Caine <nolancaine@gmail.com>
2021-12-05 17:23:25 -0700

    Add notes

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,6 +6,12 @@ const Gallery = (): JSX.Element => {
   // eventually we'll be mapping over arrays of
     // Resources and Folder in the cache
 
+  // query GET_FOLDER for whatever level you're going to
+  // render its childFolders and childResources
+  // initial visit queries for user and user's root folder
+  // store user in cache & use for all future queries
+  
+
   return (
     <section className='gallery'>
       <Resource />
