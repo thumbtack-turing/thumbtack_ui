84f7d73c5938d6a598d380bac235209751b84741
Nolan Caine <nolancaine@gmail.com>
2021-12-05 18:56:24 -0700

    Comment out sample code

--- src/client/cache.tsx
+++ src/client/cache.tsx
@@ -1,19 +1,19 @@
 import { InMemoryCache, makeVar } from '@apollo/client';
-import { Todos } from '../models/todo';
+// import { Todos } from '../models/todo';
 
 const cache: InMemoryCache = new InMemoryCache();
 
-const todosInitialValue: Todos = [
-  {
-    id: 0,
-    completed: false,
-    text: "Use Apollo Client 3"
-  }
-]
-
-// Create the todos var and initialize it with the initial value
-export const todosVar = makeVar<Todos>(
-  todosInitialValue
-);
+// const todosInitialValue: Todos = [
+//   {
+//     id: 0,
+//     completed: false,
+//     text: "Use Apollo Client 3"
+//   }
+// ]
+//
+// // Create the todos var and initialize it with the initial value
+// export const todosVar = makeVar<Todos>(
+//   todosInitialValue
+// );
 
 export default cache;
