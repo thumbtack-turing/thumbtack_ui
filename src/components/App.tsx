c8b705312f00f3b23a75891b75f0174ecef87849
Nolan Caine <nolancaine@gmail.com>
2021-12-06 23:35:53 -0700

    Add test queries to mock out user flow

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../operations/queries/GET_USER';
 import Header from './Header';
 
 const App = () => {
+  const email = 'rowan@test.com';
+  const { loading, error, data: userData } = useQuery(GET_USER, {
+    variables: { email }
+  });
+
   return (
     <>
       <Header />
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+// import { useQuery } from '@apollo/client';
+// import { GET_USER } from '../operations/queries/GET_USER';
 
 const LandingPage = () => {
+  // const email = 'rowan@test.com';
+  // const { loading, error, data: userData } = useQuery(GET_USER, {
+  //   variables: { email }
+  // });
+
   return (
     <div>sup, i'm the landing page</div>
   )
