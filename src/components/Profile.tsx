745965b0a69e59aaec48c5f6e2b650796f2520be
Nolan Caine <nolancaine@gmail.com>
2021-12-06 23:35:53 -0700

    Add test queries to mock out user flow

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,34 +1,28 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useApolloClient } from '@apollo/client';
 import { userVar, currentFolderVar } from '../client/cache';
 import AddForm from './AddForm';
 import Gallery from './Gallery';
 import { GET_USER } from '../operations/queries/GET_USER';
-import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
+// import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
 
 const Profile = (): JSX.Element => {
   // move this query to Landing Page's Login button onClick handler
-
+  // will need to pass a prop from login's getUser query
   // can we cache this data instead of prop drilling?
+  const client = useApolloClient();
+  console.log(client);
+
   const email = 'rowan@test.com';
   const { loading, error, data: userData } = useQuery(GET_USER, {
     variables: { email }
   });
+  // can't pass email as a prop to profile because of routes...
 
   const userId = userData?.getUser?.id;
   const userName = userData?.getUser?.name;
-  const user = userVar(userData?.getUser);
-  console.log(user);
-
-  const { data: folderData } = useQuery(GET_FOLDER, {
-    skip: !userId,
-    variables: { id: userId }
-  });
-
-  const currentFolderId = folderData?.getFolder?.id;
-  const childFolders = folderData?.getFolder?.childFolders;
-  const childResources = folderData?.getFolder?.childResources;
-  const currentFolder = currentFolderVar(folderData?.getFolder);
+  const childFolders = userData?.getUser?.baseFolder.childFolders;
+  const childResources = userData?.getUser?.baseFolder.childResources;
 
   return (
     <main className='profile'>
