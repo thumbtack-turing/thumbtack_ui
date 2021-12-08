b2a11f004ef53a963b183d9f937ae8accb765b90
Nolan Caine <nolancaine@gmail.com>
2021-12-07 19:29:27 -0700

    Add baseFolder and filePath

--- src/models/Folder.tsx
+++ src/models/Folder.tsx
@@ -5,6 +5,7 @@ export interface Folder {
   name: string;
   base: boolean;
   parentId: number | null;
+  filePath: string;
   childFolders: Folder[];
   childResources: Resource[];
 }
--- src/models/User.tsx
+++ src/models/User.tsx
@@ -2,6 +2,7 @@ export interface User {
   id: number | null;
   name: string;
   email: string;
+  baseFolder: object;
 }
 
 // export type Folders = Folder[];
