4e97906a9dd34e9600531445bbfa0ab453ce05ae
Nolan Caine <nolancaine@gmail.com>
2021-12-08 00:21:29 -0700

    Replace Gallery with split components

---
+++ src/components/BaseFolderView.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery, useReactiveVar } from '@apollo/client';
+import { userVar, currentFolderVar } from '../client/cache';
+import Resource from './Resource';
+import Folder from './Folder';
+import { Folder as FolderModel } from '../models/Folder';
+import { Resource as ResourceModel } from '../models/Resource';
+import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
+
+const BaseFolderView = (): JSX.Element => {
+  const user = useReactiveVar(userVar);
+  const baseFolderData = user.baseFolder;
+  console.log('baseFolderData', baseFolderData);
+
+  const childFolderElements = baseFolderData?.childFolders.map((folder: FolderModel) =>
+    <Folder key={ folder.id } { ...folder } />
+  )
+  const childResourceElements = baseFolderData?.childResources.map((resource: ResourceModel) =>
+    <Resource key={ resource.id } { ...resource } />
+  )
+
+  return (
+    <section className='gallery'>
+      <h1> { baseFolderData?.name } </h1>
+      { childFolderElements }
+      { childResourceElements }
+    </section>
+  )
+};
+
+export default BaseFolderView;
---
+++ src/components/SubFolderView.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery, useReactiveVar } from '@apollo/client';
+import { userVar, currentFolderVar } from '../client/cache';
+import Resource from './Resource';
+import Folder from './Folder';
+import { Folder as FolderModel } from '../models/Folder';
+import { Resource as ResourceModel } from '../models/Resource';
+import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
+
+const SubFolderView = (): JSX.Element => {
+  const { folderId } = useParams();
+  console.log(folderId);
+
+  const { loading, error, data } = useQuery(GET_FOLDER, {
+    variables: { id: folderId }
+  });
+
+  const folderData = data?.getFolder;
+  console.log(folderData)
+  // if (folderData) {
+  //   currentFolderVar(folderData);
+  // }
+  // const currentFolder = useReactiveVar(currentFolderVar);
+  // console.log(currentFolder)
+
+  const childFolderElements = folderData?.childFolders?.map((folder: FolderModel) =>
+    <Folder key={ folder.id } { ...folder } />
+  )
+  const childResourceElements = folderData?.childResources?.map((resource: ResourceModel) =>
+    <Resource key={ resource.id } { ...resource } />
+  )
+
+return (
+  <section className='gallery'>
+    <h1> { folderData?.name } </h1>
+    { childFolderElements }
+    { childResourceElements }
+  </section>
+)
+};
+
+export default SubFolderView;
