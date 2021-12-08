ba59662ab1a6b97c0d734286a135d213621dfa7f
Nolan Caine <nolancaine@gmail.com>
2021-12-08 00:22:24 -0700

    Use id for Link - fixes routing

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useReactiveVar } from '@apollo/client';
+import { useQuery, useReactiveVar } from '@apollo/client';
 import { currentFolderVar } from '../client/cache';
+import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
 
 import { Folder as FolderProps } from '../models/Folder';
 
 const Folder = (props: FolderProps): JSX.Element => {
-  const currentFolder = useReactiveVar(currentFolderVar);
+  // const currentFolder = useReactiveVar(currentFolderVar);
+  const { id, name } = props;
+
+  const { loading, error, data } = useQuery(GET_FOLDER, {
+    variables: { id }
+  });
+
+  const completeFolderData = data?.getFolder;
+
+  const handleClick = () => {
+    currentFolderVar(completeFolderData);
+  }
 
   return (
-    <Link to={ `${props.name}` } onClick={ () => currentFolderVar(props) } >
+    <Link to={ `${id}` } onClick={ handleClick } >
       <article className='folder'>
-        <h3>{ props.name }</h3>
+        <h3>{ name }</h3>
       </article>
     </Link>
   )
