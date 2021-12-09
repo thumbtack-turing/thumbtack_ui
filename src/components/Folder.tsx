22d1d19473b2519af9537727eaf1213880858cfd
Nolan Caine <nolancaine@gmail.com>
2021-12-09 01:02:31 -0700

    Incorporate delete functionality

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { currentFolderVar } from '../client/cache';
 import { GET_FOLDER } from '../operations/queries/GET_FOLDER';
-
+import DELETE_FOLDER from '../operations/mutations/DELETE_FOLDER';
 import { Folder as FolderProps } from '../models/Folder';
+import closeIcon from '../assets/button-close-icon-645944.png';
 
 const Folder = (props: FolderProps): JSX.Element => {
   const { id, name } = props;
 
   const { data } = useQuery(GET_FOLDER, {
+    variables: { id }
+  });
+
+  const [ deleteFolder ] = useMutation(DELETE_FOLDER, {
     variables: { id }
   });
 
@@ -19,12 +24,26 @@ const Folder = (props: FolderProps): JSX.Element => {
     currentFolderVar(completeFolderData);
   }
 
+  const handleClose = () => {
+    deleteFolder();
+    console.log('folder id', id);
+  }
+
   return (
-    <Link to={ `/myfolders/${id}` } onClick={ handleClick } className='folder-link'>
-      <article className='folder'>
-        <h3>{ name }</h3>
-      </article>
-    </Link>
+    <article className='folder-container'>
+      <input
+        type='image'
+        src={ closeIcon }
+        alt='close icon'
+        onClick={ handleClose }
+        className='close-icon'
+      />
+      <Link to={ `/myfolders/${id}` } onClick={ handleClick } className='folder-link'>
+        <article className='folder'>
+          <h3>{ name }</h3>
+        </article>
+      </Link>
+    </article>
   )
 }
 
--- src/components/Resource.tsx
+++ src/components/Resource.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-
+import { useMutation } from '@apollo/client';
 import { Resource as ResourceProps } from '../models/Resource';
+import closeIcon from '../assets/button-close-icon-645944.png';
+import DELETE_RESOURCE from '../operations/mutations/DELETE_RESOURCE';
 
 const Resource = ({
   id, name, url, image, createdAt
 }: ResourceProps): JSX.Element => {
+  const [ deleteResource, { loading, error, data } ] = useMutation(DELETE_RESOURCE, {
+    variables: { id }
+  });
+
+  const handleClose = () => {
+    deleteResource();
+    console.log('resource id', id);
+  }
+
   return (
-    <a href={ url } target='_blank' rel='noopener noreferrer' className='resource-link'>
-      <article className='resource' style={{ backgroundImage: `url(${image})` }}>
-        <h3>{ name }</h3>
-      </article>
-    </a>
+    <article className='resource-container'>
+      <input
+        type='image'
+        src={ closeIcon }
+        alt='close icon'
+        onClick={ handleClose }
+        className='close-icon'
+      />
+      <a href={ url } target='_blank' rel='noopener noreferrer' className='resource-link'>
+        <article className='resource' style={{ backgroundImage: `url(${image})` }}>
+          <h3>{ name }</h3>
+        </article>
+      </a>
+    </article>
   )
 }
 
