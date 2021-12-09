d829ec480e6d9ce1ba4d992d7adacdf089a340c0
Nolan Caine <nolancaine@gmail.com>
2021-12-09 09:34:18 -0700

    Resize/reposition text

--- src/components/SubFolderView.tsx
+++ src/components/SubFolderView.tsx
@@ -34,13 +34,25 @@ const SubFolderView = (): JSX.Element => {
     <Resource key={ resource.id } { ...resource } />
   )
 
+  const promptWhenEmpty = () => {
+    if (!childFolderElements.length && !childResourceElements.length) {
+      return (
+        <h2 className='prompt-when-empty'>
+          Nothing saved yet. Add a new resource or folder to get started!
+        </h2>
+      )
+    }
+
+  }
+
 return (
   <section className='gallery'>
     { loading && <Loading /> }
     { error && <Error /> }
     { data && <>
-      <h1 className='folder-name'> { folderData?.name } </h1>
+      <h4 className='folder-name'> /{ folderData?.name } </h4>
       <section className='gallery-items'>
+      { promptWhenEmpty() }
       { childFolderElements }
       { childResourceElements }
       </section>
