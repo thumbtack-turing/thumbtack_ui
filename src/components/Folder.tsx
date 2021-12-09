6bd47b440d50e5fb7e8b8c0d970605dd318b29b9
Nolan Caine <nolancaine@gmail.com>
2021-12-08 20:16:44 -0700

    Update link to avoid recursion

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -21,7 +21,7 @@ const Folder = (props: FolderProps): JSX.Element => {
   }
 
   return (
-    <Link to={ `${id}` } onClick={ handleClick } >
+    <Link to={ `/myfolders/${id}` } onClick={ handleClick } >
       <article className='folder'>
         <h3>{ name }</h3>
       </article>
