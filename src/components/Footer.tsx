296e5e99d261b88721a2a20d1d2138467afeee4e
Janika Hortizuela <jhortizuela21@gmail.com>
2021-12-14 13:52:22 -0700

    complete hover style on fake icons/buttons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,38 +4,61 @@ import google from '../assets/google.png'
 import apple from '../assets/apple.png'
 
 const Footer = () => {
-  const [isOver, setIsOver] = useState(false)
+  const [ isOverBtn, setIsOverBtn ] = useState(false)
+  const [ isOverLink, setIsOverLink ] = useState(false)
 
-  const handleEnter = () => {
-    setIsOver(true)
+  const handleEnterBtn = (e: any) => {
+    e.preventDefault()
+    setIsOverBtn(true)
   }
 
-  const handleLeave = () => {
-    setIsOver(false)
+  const handleLeaveBtn = (e: any) => {
+    e.preventDefault()
+    setIsOverBtn(false)
   }
 
-  const fakeButtons = isOver ? 
-  <div className="fake-hover">
+  const handleEnterLink = (e: any) => {
+    e.preventDefault()
+    setIsOverLink(true)
+  }
+
+  const handleLeaveLink = (e: any) => {
+    e.preventDefault()
+    setIsOverLink(false)
+  }
+
+  const fakeButtons = isOverBtn ? 
+
+  <div className="fake-btn">
     <p>
       This is not a real app. This app was created as a capstone project by Turing Students
     </p>
   </div> :
   null
 
+  const fakeLinks = isOverLink ?
+    <div className="fake-links">
+      <p>
+        This is not a real app. This app was created as a capstone project by Turing Students
+      </p>
+    </div> :
+    null
+
   return (
     <footer>
       <section className="left-side-footer">
+        {fakeLinks}
         <img src={thumbtack} alt="thumbtack logo" className="thumbtack-footer" />
         <div className="links-top">
-          <p className="footer-link">Download</p>
-          <p className="footer-link">License</p>
+          <p className="footer-link" onMouseEnter={handleEnterLink} onMouseLeave={handleLeaveLink}>Download</p>
+          <p className="footer-link" onMouseEnter={handleEnterLink} onMouseLeave={handleLeaveLink}>License</p>
         </div>
         <div className="links-bottom">
           <p className="footer-link">About</p>
           <p className="footer-link">Features</p>
-          <p className="footer-link">Pricing</p>
-          <p className="footer-link">Careers</p>
-          <p className="footer-link">Privacy Policy</p>
+          <p className="footer-link" onMouseEnter={handleEnterLink} onMouseLeave={handleLeaveLink}>Pricing</p>
+          <p className="footer-link" onMouseEnter={handleEnterLink} onMouseLeave={handleLeaveLink}>Careers</p>
+          <p className="footer-link" onMouseEnter={handleEnterLink} onMouseLeave={handleLeaveLink}>Privacy Policy</p>
         </div>
         <div>
          <p className="footer-link">© 2021 Thumbtack. All rights reserved</p>
@@ -44,8 +67,8 @@ const Footer = () => {
       <section className="get-app">
         <p>Get the App</p>
         {fakeButtons}
-        <img src={apple} alt="apple store" className="fake-icon" onMouseEnter={handleEnter} onMouseLeave={handleLeave}/>
-        <img src={google} alt="google store" className="fake-icon" />
+        <img src={apple} alt="apple store" className="fake-icon" onMouseEnter={handleEnterBtn} onMouseLeave={handleLeaveBtn}/>
+        <img src={google} alt="google store" className="fake-icon" onMouseEnter={handleEnterBtn} onMouseLeave={handleLeaveBtn}/>
       </section>
     </footer>
   )
--- src/components/LandingBlock1.tsx
+++ src/components/LandingBlock1.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import book from '../assets/book.mp4'
-import decor1 from '../assets/decor1.png'
-import decor2 from '../assets/decor2.png'
-import decor3 from '../assets/decor3.png'
 import "animate.css/animate.min.css"
 
 
@@ -10,13 +7,10 @@ const LandingBlock1 = () => {
   return (
     <div className="block1">
       <div className="videoContainer">
-        <img src={decor1} alt="decor" className="decor animate__animated animate__fadeInUp" id="decor1"/>
-        <img src={decor2} alt="decor2" className="decor animate__animated animate__fadeInUp animate__slower" id="decor2"/>
-        <img src={decor3} alt="decor3" className="decor animate__animated animate__fadeInUp" id="decor3"/>
         <div className="description-1">
           <h1 className="catchphrase animate__animated animate__fadeInUp animate__slow">The smartest way to organize your research</h1>
           <h2 className="catchphrase2 animate__animated animate__fadeInUp animate__slower">Never use bookmarks again!</h2>
-          <button className="block1-btn animate__animated animate__fadeInUp animate__slower">Let's Go!</button>
+          <button className="go-to-app">Let's Go!</button>
         </div>
         <div className="overlay"></div>
         <video autoPlay={true} muted loop>
