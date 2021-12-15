e21d1c367acdfa201e052889e39222b3fbdbc218
Nolan Caine <nolancaine@gmail.com>
2021-12-14 18:15:21 -0700

    Add error type

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Error = ({ error }: any): JSX.Element => {
-  console.log(error)
+const Error = ({ type = 'none' }: any): JSX.Element => {
+  console.log(type)
+  let errorType;
+  if (type === 'none') {
+    return <h2>Hmm, something went wrong. Try refreshing your browser.</h2>
+  } else if (type === 'invalid url') {
+    return <h2>Please make sure to enter a full valid url, including "https://"! Hit refresh to try again.</h2>
+  }
+
   return (
     <article>
       <h2>Hmm, something went wrong. Try refreshing your browser.</h2>
